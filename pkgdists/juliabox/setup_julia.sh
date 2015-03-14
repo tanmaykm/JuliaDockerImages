011c2d7bcdd0494a33509a84f753e4b7c49179c9
tan <tanmaykm@gmail.com>
2015-03-14 18:37:02 +0530

    add julia 0.3.6 and juliabox package bundle

---
+++ pkgdists/juliabox/setup_julia.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DEFAULT_PACKAGES="IJulia PyPlot SIUnits Gadfly DataStructures HDF5 MAT \
+Iterators NumericExtensions SymPy Interact Roots \
+DataFrames RDatasets Distributions SVM Clustering GLM \
+Optim JuMP GLPKMathProgInterface Clp NLopt Ipopt \
+Cairo GraphViz \
+Images ImageView WAV ODE Sundials LinearLeastSquares \
+BayesNets PGFPlots GraphLayout \
+Stan \
+Patchwork Quandl Lazy QuantEcon"
+
+for pkg in ${DEFAULT_PACKAGES}
+do
+    echo ""
+    echo "Adding default package $pkg"
+    julia -e "Pkg.add(\"$pkg\")"
+done
+
+INTERNAL_PACKAGES="https://github.com/shashi/Homework.jl.git \
+https://github.com/tanmaykm/JuliaBox.jl.git"
+
+for pkg in ${INTERNAL_PACKAGES}
+do
+    echo ""
+    echo "Adding internal package $pkg"
+    julia -e "Pkg.clone(\"$pkg\")"
+done
+
+julia -e "Pkg.checkout(\"Interact\")"
+
+echo ""
+echo "Creating package list..."
+julia -e "Pkg.status()" > /.juliabox/packages.txt
